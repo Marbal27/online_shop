7a297981a672b03cb24816c457b0c0ad3e5e4b0d
Marbal27 <Marbel27@yandex.ru>
2022-12-28 11:13:08 +0500

    новый компонент

--- src/components/CartPage.vue
+++ src/components/CartPage.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="cart-page">
         <h1>hello</h1>
+        <cart-item></cart-item>
     </div>
 </template>
 
 <script>
+import CartItem from './CartItem.vue'
+
 export default {
     name: "CartPage",
-
+    components: {
+        CartItem,
+    }
 }
 </script>
 
